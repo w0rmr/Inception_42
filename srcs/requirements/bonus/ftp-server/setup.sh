438de2ff5786d16c66d9490ce5487e0ab48b364e
Achraf Toukmati <atoukmat@c2r2p4.1337.ma>
2024-11-04 15:06:28 +0100

    ysalam

--- srcs/requirements/bonus/ftp-server/setup.sh
+++ srcs/requirements/bonus/ftp-server/setup.sh
@@ -14,32 +14,28 @@ chown $FTP_USER:$FTP_USER /home/$FTP_USER/ftp/files
 mkdir -p /var/run/vsftpd/empty
 chmod 755 /var/run/vsftpd/empty
 
-if [-f "flag" ]; then 
-    echo "config howa hadak :( "
-else 
-    touch flag 
-    echo "
-    listen=YES
-    listen_ipv6=NO
-    anonymous_enable=NO
-    local_enable=YES
-    write_enable=YES
-    local_umask=022
-    dirmessage_enable=YES
-    use_localtime=YES
-    xferlog_enable=YES
-    connect_from_port_20=YES
-    chroot_local_user=YES
-    allow_writeable_chroot=YES
-    pasv_enable=YES
-    pasv_min_port=50000
-    pasv_max_port=50005
-    userlist_enable=YES
-    userlist_file=/etc/vsftpd.userlist
-    userlist_deny=NO
-    secure_chroot_dir=/var/run/vsftpd/empty
-    local_root=/home/$FTP_USER/ftp
-    " >> /etc/vsftpd.conf
-fi
+echo "
+listen=YES
+listen_ipv6=NO
+anonymous_enable=NO
+local_enable=YES
+write_enable=YES
+local_umask=022
+dirmessage_enable=YES
+use_localtime=YES
+xferlog_enable=YES
+connect_from_port_20=YES
+chroot_local_user=YES
+allow_writeable_chroot=YES
+pasv_enable=YES
+pasv_min_port=50000
+pasv_max_port=50005
+userlist_enable=YES
+userlist_file=/etc/vsftpd.userlist
+userlist_deny=NO
+secure_chroot_dir=/var/run/vsftpd/empty
+local_root=/home/$FTP_USER/ftp
+" >> /etc/vsftpd.conf
+
 
 exec vsftpd
--- srcs/requirements/bonus/static-website/setup.sh
+++ srcs/requirements/bonus/static-website/setup.sh
@@ -1,19 +1,15 @@
 #! /bin/bash
-if [ -d "/home/$SSH_USER"]; then
-    echo "USER exist" 
-else  
-    adduser $SSH_USER << EOF
-    $SSH_PASS
-    $SSH_PASS
+adduser $SSH_USER << EOF
+$SSH_PASS
+$SSH_PASS
 EOF
-    mkdir -p /home/$SSH_USER/files
-    mv files/* /home/$SSH_USER/files/
-    chown -R $SSH_USER:$SSH_USER /home/$SSH_USER/files
-    chmod 755 /home/$SSH_USER/files
-    chmod -R 644 /home/$SSH_USER/files/*
+mkdir -p /home/$SSH_USER/files
+mv files/* /home/$SSH_USER/files/
+chown $SSH_USER:$SSH_USER /home/$SSH_USER/files
+cd /home/$SSH_USER/files
+chmod 700 /home/$SSH_USER/files
 
-    echo "Port  $SSH_PORT" >> /etc/ssh/sshd_config
-    echo "Port  $SSH_PORT" >> /etc/ssh/ssh_config
-    service  ssh restart
-fi
+echo "Port  $SSH_PORT" >> /etc/ssh/sshd_config
+echo "Port  $SSH_PORT" >> /etc/ssh/ssh_config
+service  ssh restart
 exec $@
